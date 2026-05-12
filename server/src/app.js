9ee3a8627ff7b2dda7062ccc1330a51da4aabb88
ParasWaghela07 <paraswaghela777@gmail.com>
2026-05-12 22:39:55 +0530

    local and global rate limiter added

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,23 @@
 import express from "express";
 import cors from "cors";
-
+import helmet from "helmet";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import dashboardRoutes from "./routes/dashboard.routes.js";
+import {
+  apiLimiter
+}
+from "./middlewares/rateLimit.middleware.js";
 
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
-
 app.use(express.json());
+app.use(apiLimiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
