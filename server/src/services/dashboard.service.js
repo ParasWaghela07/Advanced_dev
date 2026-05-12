05dbcb91fe44bf8b0a417f372fddeac1d7a820b4
ParasWaghela07 <paraswaghela777@gmail.com>
2026-05-13 02:13:43 +0530

    redis added

--- server/src/services/dashboard.service.js
+++ server/src/services/dashboard.service.js
@@ -1,70 +1,77 @@
 import Project from "../models/project.model.js";
+import redisClient
+from "../config/redis.js";
 
-export const getDashboardStatsService = async (
-  userId,
-  userRole
-) => {
+export const getDashboardStatsService =
+async (userId, userRole) => {
 
-  // 🔐 admin sees all projects
+  // 🔑 unique cache key
+  const cacheKey =
+    `dashboard:${userId}:${userRole}`;
+
+  // 🔍 check cache
+  const cachedData =
+    await redisClient.get(cacheKey);
+
+  if (cachedData) {
+
+    console.log("Cache HIT");
+
+    return JSON.parse(cachedData);
+
+  }
+
+  console.log("Cache MISS");
+
+  // DB query
   const matchStage =
     userRole === "admin"
       ? {}
       : { owner: userId };
 
-  // 📊 aggregation pipeline
-  const stats = await Project.aggregate([
+  const stats =
+    await Project.aggregate([
+      {
+        $match: matchStage
+      },
 
-    {
-      $match: matchStage
-    },
+      {
+        $facet: {
 
-    {
-      $facet: {
+          totalProjects: [
+            {
+              $count: "count"
+            }
+          ],
 
-        // total projects
-        totalProjects: [
-          {
-            $count: "count"
-          }
-        ],
-
-        // projects grouped by status
-        projectsByStatus: [
-          {
-            $group: {
-              _id: "$status",
-              count: {
-                $sum: 1
+          projectsByStatus: [
+            {
+              $group: {
+                _id: "$status",
+                count: {
+                  $sum: 1
+                }
               }
             }
-          }
-        ],
+          ]
 
-        // recent projects
-        recentProjects: [
-          {
-            $sort: {
-              createdAt: -1
-            }
-          },
-
-          {
-            $limit: 5
-          },
-
-          {
-            $project: {
-              title: 1,
-              status: 1,
-              createdAt: 1
-            }
-          }
-        ]
-
+        }
       }
+
+    ]);
+
+  // 💾 store in cache
+  await redisClient.set(
+
+    cacheKey,
+
+    JSON.stringify(stats[0]),
+
+    {
+      EX: 60
     }
 
-  ]);
+  );
 
   return stats[0];
 
