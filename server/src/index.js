5d1235ce466969a8d7695b9f4a3244b16b2fbbe8
ParasWaghela07 <paraswaghela777@gmail.com>
2026-05-14 10:20:17 +0530

    socket added

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,58 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
+import http from "http";
+
 import app from "./app.js";
+
+import { Server } from "socket.io";
+
 import { connectDB } from "./config/db.js";
 
-import { connectRedis }
-from "./config/redis.js";
-
-
+import { connectRedis } from "./config/redis.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+await connectDB();
+
 await connectRedis();
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+
+  console.log(
+    `User Connected: ${socket.id}`
+  );
+
+  // 🔥 join personal room
+  socket.on("joinRoom", (userId) => {
+
+    socket.join(userId);
+
+    console.log(
+      `User joined room: ${userId}`
+    );
+
+  });
+
+  socket.on("disconnect", () => {
+
+    console.log(
+      `User Disconnected: ${socket.id}`
+    );
+
+  });
+
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
